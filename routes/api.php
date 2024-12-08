57ccb5d5a531a83ce1f91385e190c43388f37e0c
nabeel-devs <nabeel33@apnadevs.com>
2024-12-08 19:20:55 +0500

    updated guard

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::group(['prefix' => 'user'], function () {
         Route::get('/{contact}', 'show')->name('contacts.show');
     });
 
-    Route::middleware(['auth:user'])->group(function () {
+    Route::middleware(['auth'])->group(function () {
 
         Route::post('/email/verify', [App\Http\Controllers\api\user\EmailVerificationController::class, 'verify']);
         Route::post('/email/resend', [App\Http\Controllers\api\user\EmailVerificationController::class, 'resend']);
