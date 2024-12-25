caa00e32868d41838e231e6125b0944e02fce3b5
nabeel-devs <nabeel33@apnadevs.com>
2024-12-25 19:22:45 +0500

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-upload', function () {
+    Storage::disk('s3')->put('test.txt', 'Testing connection');
+    return 'File uploaded successfully!';
 });
