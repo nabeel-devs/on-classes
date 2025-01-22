f0135bb2f1617adcc4abf0de822f1499d1b6ec66
nabeel-devs <nabeel33@apnadevs.com>
2025-01-22 23:56:54 +0500

    added admins

---
+++ app/Models/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens, Notifiable;  // Use Notifiable for email notifications (optional)
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+}
